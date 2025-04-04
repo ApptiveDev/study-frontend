16e762cac643a3376c91b70e0caf77adf3602316
sssssoyoung <thdual924@naver.com>
2025-04-04 17:51:54 +0900

    2주차 과제

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+//import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="container">
+      <Header title={"윈도우 정품 인증"} />
+      <main className="content">
+        <h1>Hello<br/>World!!!</h1>
+        <h2 onClick={() => setCount(count+1)}>{count}</h2> 
+        <div className="widget-container">
+          <div className="widget">
+            <div className="widget-box">1</div>
+            <div className="widget-box">2</div>
+            <div className="widget-box">3</div>
+            <div className="widget-box">4</div>
+            <div className="widget-box">5</div>
+          </div>
+        </div>
+      </main>
+      <nav className="nav">
+        <div className='nav-boxes'>
+          <div className="nav-box"></div>
+          <div className="nav-box"></div>
+          <div className="nav-box"></div>
+          <div className="nav-box"></div>
+        </div>
+      </nav>
+    </div>
     </>
   )
 }
