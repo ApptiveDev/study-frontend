12ef3fb47a1a8c61f94f2812321b580c48ed2292
yuhoyeong <168501145+yuhoyeong@users.noreply.github.com>
2025-05-13 20:44:24 +0900

    stopwatch

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -6,21 +6,32 @@ const Stopwatch = () => {
   const intervalRef = useRef(null);
 
   const handleStart = () => {
-    Date.now();
-    startTieme = Date.now();
+    const start = Date.now();
+    setStartTime(start);
+    setNow(start);
+    
+    intervalRef.current = setInterval(() => {
+      setNow(Date.now());
+    }, 10);
     // TODO1: 시작 시각을 설정하세요.
     // TODO1: 시작 시각과 현재 시각을 `Date.now()`로 설정하세요.
     // TODO2: `setInterval`을 사용하여 `now`가 10ms 주기로 업데이트 되도록 하세요.
     // TODO3: interval id는 `intervalRef`에 저장하여 렌더링을 방지합니다.
+  } 
+  
+  let secondsPassed = 0;
+  if (startTime && now) {
+    secondsPassed = (now - startTime) / 1000;
   }
+  // TODO4: `startTime`과 `now`가 null이 아닐 때만 경과 시간을 계산하세요.
+
 
   const handleStop = () => {
+    clearInterval(intervalRef.current);
     // TODO 5: `intervalRef`를 사용하여 타이머를 중지하세요.
   }
 
-  let secondsPassed = 0;
-  // TODO4: `startTime`과 `now`가 null이 아닐 때만 경과 시간을 계산하세요.
-
+ 
   return (
     <>
       <h1>Time passed: {secondsPassed.toFixed(3)}</h1>
