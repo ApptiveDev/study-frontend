01770353297d43bd35506374eb31e11190847025
mingkyeongg <ming0820@naver.com>
2025-05-27 11:31:57 +0900

    feat: 커스텀 훅 과제 셋팅

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  const { width, height } = ...?
+  return (
+    <>
+      <div>
+      <p>Window width: {width}px</p>
+      <p>Window height: {height}px</p>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/hooks/useWindowSize.js
@@ -0,0 +1,5 @@
+const useWindowSize = () => {
+  // 작성해주세요.
+}
+
+export default useWindowSize;
