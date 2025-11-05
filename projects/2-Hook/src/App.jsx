beb74a9ceadfb9662ba341bdfdc40d9170b19bf0
인성 <me@insd.dev>
2025-11-05 14:47:54 +0900

    feat: 타이머 구조 작성

--- projects/2-Hook/src/App.jsx
+++ projects/2-Hook/src/App.jsx
@@ -1,10 +1,8 @@
-import './index.css';
-
 function App() {
   return (
-    <div className="w-100 border rounded-xl overflow-hidden">
-      <div className="max-h-100 overflow-y-auto">
-
+    <div className="w-80 border rounded-xl overflow-hidden">
+      <div className="max-h-100 overflow-y-auto flex flex-col items-center py-4">
+        <p className="font-jetbrains-mono text-xl">00:00:00</p>
       </div>
     </div>
   );
