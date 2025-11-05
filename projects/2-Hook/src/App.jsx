38842cfd2d2a228895f09bc7969fc6504362e10c
인성 <me@insd.dev>
2025-11-05 13:57:47 +0900

    chore: 과제 2번 부트스트랩

---
+++ projects/2-Hook/src/App.jsx
@@ -0,0 +1,13 @@
+import './index.css';
+
+function App() {
+  return (
+    <div className="w-100 border rounded-xl overflow-hidden">
+      <div className="max-h-100 overflow-y-auto">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
