8b5a1cd73d1b40e70004f9c8a9ec28115d25e6f1
yuhoyeong <marcus8213@pusan.ac.kr>
2025-11-11 23:05:20 +0900

    과제 1 제출

--- projects/1-react/src/App.jsx
+++ projects/1-react/src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react';
 import './index.css';
+import Placeholder from './components/placeholder';
+import TodoInput from './components/input';
+import TodoItem from './components/item';
 
 // 여기에 코드를 작성하세요
 
@@ -8,23 +11,37 @@ function App() {
 
   function addTodo(text) {
     // 여기에 코드를 작성하세요
+    todos.push({ id: Date.now(), text, completed: false });
+    setTodos([...todos]);
   }
 
   function toggleTodo(id) {
     // 여기에 코드를 작성하세요
+    const todo = todos.find((todo) => todo.id === id);
+    todo.completed = !todo.completed;
+    setTodos([...todos]);
   }
 
   function deleteTodo(id) {
     // 여기에 코드를 작성하세요
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
   }
 
   return (
     <div className="w-100 border rounded-xl overflow-hidden">
       <div className="max-h-100 overflow-y-auto">
-        {/* 여기에 코드를 작성하세요 */}
+        <TodoInput onAdd={addTodo} />
+        {todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            onToggle={toggleTodo}
+            onDelete={deleteTodo}
+          />
+        ))}
       </div>
-
-      {/* 여기에 코드를 작성하세요 */}
+      {todos.length === 0 && <Placeholder />}
     </div>
   );
 }
