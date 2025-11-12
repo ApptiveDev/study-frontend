5b8f29a4005b570b6efd5a88f40400395a74ab84
gayeoniya <cindy20269405@gmail.com>
2025-11-12 18:28:43 +0900

    todoList 과제 수정

--- projects/1-react/src/App.jsx
+++ projects/1-react/src/App.jsx
@@ -1,63 +1,9 @@
 import { useState } from 'react';
 import './index.css';
+
+import TodoInput from './components/input';
+import TodoItem from './components/item';
 import Placeholder from './components/placeholder';
-
-function InputForm({ addTodo }) {
-  const [value, setValue] = useState('');
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!value.trim()) return;
-    addTodo(value);
-    setValue('');
-  };
-
-  return (
-    <form onSubmit={handleSubmit} className="flex items-center border-t p-4">
-      <div className="w-5 h-5 mr-4"></div>
-
-      <input
-        type="text"
-        className="flex-grow outline-none bg-transparent"
-        placeholder="새로운 할 일을 입력하세요..."
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-      />
-      <button
-        type="submit"
-        className="text-red-500 hover:text-red-700 font-bold ml-4 px-2 py-1"
-      >
-        추가
-      </button>
-    </form>
-  );
-}
-
-function TodoList({ todo, toggleTodo, deleteTodo }) {
-  return (
-    <div className="flex items-center p-4 border-b last:border-b-0 hover:bg-gray-400 transition-colors">
-      <input
-        type="checkbox"
-        className="mr-4 w-5 h-5 accent-blue-500"
-        checked={todo.complete}
-        onChange={() => toggleTodo(todo.id)}
-      />
-      <span
-        className={`flex-grow text-white ${
-          todo.complete ? 'line-through text-blue-400' : ''
-        }`}
-      >
-        {todo.text}
-      </span>
-      <button
-        onClick={() => deleteTodo(todo.id)}
-        className="text-red-500 hover:text-red-700 font-bold ml-4 px-2 py-1 transition-colors"
-      >
-        삭제
-      </button>
-    </div>
-  );
-}
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -83,8 +29,7 @@ function App() {
   }
 
   function deleteTodo(id) {
-    const updateTodo = todos.filter((todo) => todo.id !== id);
-    setTodos(updateTodo);
+    setTodos(todos.filter((todo) => todo.id !== id));
   }
 
   function filterTodo(todos, filter) {
@@ -133,7 +78,7 @@ function App() {
           <Placeholder />
         ) : (
           filterTodos.map((todo) => (
-            <TodoList
+            <TodoItem
               key={todo.id}
               todo={todo}
               toggleTodo={toggleTodo}
@@ -143,7 +88,7 @@ function App() {
         )}
       </div>
 
-      <InputForm addTodo={addTodo} />
+      <TodoInput onAdd ={addTodo} />
     </div>
   );
 }
