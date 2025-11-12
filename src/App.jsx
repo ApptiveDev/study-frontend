95814e43c18a1a369946faff468a7069ef3ec70f
yeeun <143855139+badyeun@users.noreply.github.com>
2025-11-12 19:44:40 +0900

    1-assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { ThemeProvider } from './contexts/ThemeContext'
 import Home from './pages/Home'
 
 function App() {
   // TODO: ThemeProvider로 Home 컴포넌트를 감싸주세요
   return (
+    <ThemeProvider>
       <Home />
+    </ThemeProvider>
   )
 }
 
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 
 
 const Button = () => {
-  const { toggleDarkMode, isDarkMode } = // TODO : context에서 다크 모드 상태와 토글 함수를 가져옵니다.
+  const { toggleDarkMode, isDarkMode } = use(ThemeContext);// TODO : context에서 다크 모드 상태와 토글 함수를 가져옵니다.
 
   
   const handleClick = () => {
@@ -10,9 +10,9 @@ const Button = () => {
 
   return (
     // TODO: '다크 모드'와 '라이트 모드'의 버튼 색상, 버튼의 글자 색상을 다르게 설정해주세요
-    <button style={{backgroundColor: ??,  color: ??, borderRadius: '5px', border: 'none', cursor: 'pointer', marginTop: '20px'}} onClick={handleClick}>
+    <button style={{backgroundColor: isDarkMode ? 'white': 'black',  color: isDarkMode ? 'black' : 'white', borderRadius: '5px', border: 'none', cursor: 'pointer', marginTop: '20px'}} onClick={handleClick}>
       {/* TODO : 다크 모드일 때는 "라이트 모드", 라이트 모드 일 때는 "다크 모드"로 텍스트 변경 */}
-      { ?? }
+      {  isDarkMode ? '라이트 모드' : '다크 모드'}
     </button>
   );
 }
