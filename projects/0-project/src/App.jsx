5d99c850b58cdec551e460539582263268eeee5c
yj0602 <yj0602@pusan.ac.kr>
2026-04-28 21:49:46 +0900

    0-project 과제

---
+++ projects/0-project/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='profile'>
+      <h1>노윤지</h1>
+      <img src="/profile.png" alt="프로필 사진" />
+      <p>안녕하세요!<br /> 프론트엔드를 공부하고 있는 노윤지입니다!!</p>
+      <h2>연락처</h2>
+      <p>Email: yj0602@pusan.ac.kr</p>
+      <p>010-8698-0920</p>
+    </div>
+  )
+}
+
+export default App
