761564598ed7f4b5a258e18329f4eac78151081e
ebin5780 <ebin5780@gmail.com>
2026-07-06 17:44:19 +0900

    2주차 과제

--- projects/2-Hook/src/lib/hooks/debounce.js
+++ projects/2-Hook/src/lib/hooks/debounce.js
@@ -3,7 +3,34 @@
  * 주로 검색 입력과 같이 사용자가 빠르게 입력할 때 API 호출을 줄이기 위해 사용됩니다.
  * @param {() => void} callback
  * @param {number} delay
+ * @param {any} dependencies
  */
-export default function useDebounce(callback, delay) {
-    // enter your code here
+
+import { useEffect, useRef } from "react";
+
+export default function useDebounce(callback, delay, dependencies) {
+    // Stale Closure 방지용 변수
+    // 리렌더링 시 최신 함수를 기억하기 위해 useRef 사용
+    const callbackRef = useRef(callback);
+
+    // 최신 함수 동기화
+    // 컴포넌트가 다시 그려질 때마다 ref 안의 함수를 최신 버전으로 갱신
+    // 주소값을 고정하되 안의 내용만 최신 상태로
+    useEffect(() => {
+        callbackRef.current = callback;
+    }, [callback]);
+
+    // 디바운스 타이머 제어
+    // 지정된 delay 시간이 지나면 ref에 저장된 최신 함수를 실행시키는 곳
+    useEffect(() => {
+        // 사용자가 타이핑을 시작하면 'delay'만큼 기다리는 타이머를 겸
+        const timer = setTimeout(() => {
+            callbackRef.current();  // 지정한 시간이 지나면 함수 실행
+        }, delay);
+
+        // 사용자가 입력을 멈추지 않고 계속 타이핑하면 (== dependencies가 계속 변함)
+        // 다음 useEffect가 실행되기 직전에 이 return 함수 먼저 실행
+        // 이전 타이머를 즉시 취소(clear)하여 실행을 뒤로 미룸
+        return () => clearTimeout(timer);
+    }, [delay, dependencies]);
 }
--- projects/2-Hook/src/lib/hooks/storage.js
+++ projects/2-Hook/src/lib/hooks/storage.js
@@ -4,8 +4,36 @@
  * @param {string} key
  * @param {unknown} initialValue
  */
-export default function useStorageState(key, initialValue) {
-  // enter your code here
 
-  return ['', () => {}];
+import { useState } from "react";
+import useDebounce from "./debounce"; // useDebounce 훅 불러오기
+
+export default function useStorageState(key, initialValue) {
+  // 지연 초기화
+  // useState 내부에 함수를 인자로 넘겨줌 -> 이 컴포넌트가 처음 마운트될 때 한 번만 함수 실행됨
+  // 새로고침할 때 로컬스토리지에서 기존 데이터를 꺼내와 초기 상태값으로 세팅
+  const [state, setState] = useState(() => {
+    try {
+      const saved = localStorage.getItem(key);
+      // 기존 저장된 데이터가 있다면 JSON 문자열을 원래 객체/문자열로 파싱
+      // 없으면 초기값 사용
+      return saved ? JSON.parse(saved) : initialValue;
+    } catch (error) {
+      console.error('로컬스토리지 읽기 실패:', error);
+      return initialValue;
+    }
+  });
+  // 디바운스를 활용한 로컬스토리지 자동 동기화
+  // if 사용자가 키보드를 칠 때마다(== state가 바뀔 때마다) 실시간으로 로컬 스토리지에 저장 -> 브라우저 과부화
+  // => useDebounce를 활용해 사용자가 입력을 멈추고 500ms가 지나야 저장 로직 돌도록 최적화
+  // 인자로 [state]를 넘겨주어, 실제로 글자가 바뀔 때만 타이머 작동
+  useDebounce(() => {
+    try {
+      localStorage.setItem(key, JSON.stringify(state));
+    } catch (error) {
+      console.error('로컬스토리지 저장 실패:', error);
+    }
+  }, 500, state);
+
+  return [state, setState];
 }
